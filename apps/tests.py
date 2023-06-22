41ac7874d70e58e3b78a4bbb5ee80987a05b0aa2
pythonuzbek <nusratullo3304@gmail.com>
2023-06-22 10:42:46 +0500

    change some things

--- apps/tests.py
+++ apps/tests.py
@@ -6,28 +6,23 @@ from apps.models import Category
 
 
 @pytest.mark.django_db
-def test_get_task(api_client) -> None:
-    payload = {
-        'name': 'Food'
-    }
-    url = reverse_lazy('category-list')
-    response = api_client.get(url, payload, format='json')
-    assert response.status_code == status.HTTP_200_OK
+class TestAdvert:
+    def test_create_task(self,api_client) -> None:
+        payload = {
+            'name': 'Food'
+        }
+        url = reverse_lazy('category-list')
+        response = api_client.post(url, payload)
+        print(response)
+        category = Category.objects.first()
+        assert response.status_code == status.HTTP_201_CREATED
+        assert category is not None
+        assert category.name == payload['name']
 
 
-@pytest.mark.django_db
-def test_create_task(api_client) -> None:
-    payload = {
-        'name': 'Food'
-    }
-    url = reverse_lazy('category-list')
-    response = api_client.post(url, payload)
-    print(response)
-    category = Category.objects.first()
-    assert response.status_code == status.HTTP_201_CREATED
-    assert category is not None
-    assert category.name == payload['name']
+    def test_pytest_working(self):
+        assert True == True
 
 
-def test_pytest_working():
-    assert True == True
+
+
